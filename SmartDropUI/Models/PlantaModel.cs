e6cba52c83e2636f86243d0f1f67bbf1bbda2c41
GAYLET <gmaiapaternina>
2025-11-16 23:58:40 -0500

    cambios vista 4 y 5

---
+++ SmartDropUI/Models/PlantaModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartDropUI.Models
+{
+    public class PlantaModel
+    {
+        public int Id { get; set; }
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public double TemperaturaOptima { get; set; }
+        public double HumedadOptima { get; set; }
+        public string LuzOptima { get; set; } = string.Empty;
+        public string FrecuenciaRiego { get; set; } = string.Empty;
+        public string ImagenUrl { get; set; } = string.Empty;
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+    }
+}
